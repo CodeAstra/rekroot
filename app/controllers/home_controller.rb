39a54f6b6a34547bcb5946563abcb3bb464efa1e
Rohit Reddy D <rohit.dega@gmail.com>
2015-01-22 16:58:11 +0530

    Moved the code from home controller to users controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,25 +1,11 @@
 class HomeController < ApplicationController
  
   def index
-    if user_signed_in?
-      render_dashboard
-    else 
-      render 'home/index'
-    end 
+    
   end
 
 
-  private 
-
-    def render_dashboard
-      if current_user.admin? 
-        render 'user/dashboard/admin'
-      elsif current_user.employer?
-        render 'user/dashboard/employer'
-      elsif current_user.applicant?
-        render 'user/dashboard/applicant'
-      end
-    end
+  
         
 
           
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,23 @@ class UsersController < ApplicationController
 before_action :authenticate_user!
   
   def index 
+    if user_signed_in?
+      render_dashboard
+    else 
+      render 'home/index'
+    end 
   end
+
+  private 
+
+    def render_dashboard
+      if current_user.admin? 
+        render 'user/dashboard/admin'
+      elsif current_user.employer?
+        render 'user/dashboard/employer'
+      elsif current_user.applicant?
+        render 'user/dashboard/applicant'
+      end
+    end
 
 end
