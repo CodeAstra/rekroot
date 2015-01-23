394890403c4c1699a6e4e3f0b62402439c77b5b8
Rohit Reddy D <rohit.dega@gmail.com>
2015-01-23 11:55:36 +0530

    added role to deise controller and added  signout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,16 @@ before_action :authenticate_user!
   
   def index 
     if user_signed_in?
+      
       render_dashboard
     else 
       render 'home/index'
     end 
+
+
   end
 
-  def create 
-    @user = User.new
-    render_dashboard
-  end 
+  
 
 
   private 
