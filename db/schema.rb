6c60dc0481c4e174d45a912560dd27b711d4ff3d
Rohit Reddy D <rohit.dega@gmail.com>
2015-01-25 12:16:40 +0530

    created applicant model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150124130100) do
+ActiveRecord::Schema.define(version: 20150125064619) do
+
+  create_table "applicants", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
