f919d805647abce8957d68fa23d648098a081baa
Rohit Reddy D <rohit.dega@gmail.com>
2015-02-18 16:29:32 +0530

    Merging

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,14 @@ ActiveRecord::Schema.define(version: 20150210124202) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "answer"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "question_id"
+    t.integer  "applicant_id"
   end
+
+  add_index "answers", ["applicant_id"], name: "index_answers_on_applicant_id"
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
 
   create_table "applicants", force: :cascade do |t|
     t.string   "name"
@@ -28,8 +33,8 @@ ActiveRecord::Schema.define(version: 20150210124202) do
     t.integer  "gender"
     t.string   "phone"
     t.integer  "job_id"
-    t.integer  "status",     default: 1
     t.boolean  "confirm",    default: false
+    t.integer  "status",     default: 1
     t.integer  "position",   default: 1
   end
 
@@ -64,10 +69,16 @@ ActiveRecord::Schema.define(version: 20150210124202) do
   add_index "jobs", ["company_id"], name: "index_jobs_on_company_id"
 
   create_table "questions", force: :cascade do |t|
-    t.string   "question"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "question"
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.integer  "question_type", default: 1
+    t.text     "options"
+    t.integer  "job_id"
+    t.boolean  "mandatory",     default: false
   end
+
+  add_index "questions", ["job_id"], name: "index_questions_on_job_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
