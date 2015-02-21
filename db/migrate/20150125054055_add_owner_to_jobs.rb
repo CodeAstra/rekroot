4a436d6edbb9f712046ab44e1dce82efda7b36de
Rohit Reddy D <rohit.dega@gmail.com>
2015-02-21 14:00:38 +0530

    Heroku refactoring

--- db/migrate/20150125054055_add_owner_to_jobs.rb
+++
@@ -1,5 +0,0 @@
-class AddOwnerToJobs < ActiveRecord::Migration
-  def change
-    add_reference :jobs, :owner, index: true
-  end
-end
--- db/migrate/20150203130110_add_owner_to_company.rb
+++
@@ -1,6 +0,0 @@
-class AddOwnerToCompany < ActiveRecord::Migration
-  def change
-    add_reference :companies, :owner, index: true
-    add_foreign_key :companies, :owners
-  end
-end
