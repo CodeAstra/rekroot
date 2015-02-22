57a7f53f12d6d47100c1aefd66fc802552d80241
Rohit Reddy D <rohit.dega@gmail.com>
2015-02-22 18:09:24 +0530

    Configuring mail

--- db/schema.rb
+++ db/schema.rb
@@ -80,12 +80,12 @@ ActiveRecord::Schema.define(version: 20150221113703) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
+    t.string   "email",                  default: "",         null: false
     t.string   "encrypted_password",     default: ""
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,          null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -98,7 +98,7 @@ ActiveRecord::Schema.define(version: 20150221113703) do
     t.datetime "updated_at"
     t.integer  "role",                   default: 2
     t.integer  "company_id"
-    t.string   "name"
+    t.string   "name",                   default: "Nonamion"
     t.string   "invitation_token"
     t.datetime "invitation_created_at"
     t.datetime "invitation_sent_at"
